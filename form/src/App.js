7866ec46bb185deac3a6ed8036dc8bebe6b64feb
Prathamesh-Kulkarni01 <prathameshkulkarni710@gmail.com>
2023-02-27 18:27:28 +0530

    Form components updated... display form after login added

--- form/src/App.js
+++ form/src/App.js
@@ -1,43 +1,76 @@
 import { useState } from "react";
+import DisplayData from "./component/DisplayData";
 import "./styles.css";
-
+import uuid from 'react-uuid';
 export default function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+
+  const [formData, setFormData] = useState({
+    name:"",
+    password:"",
+    city:"",
+    server:"",
+    role:"",
+    service:[]
+
+  })
+
+const checkList=["Mail","Payroll","Self-service"]
+const radioList=["Admin","Engineer","Manager","Guest"]
+  
+
   const [nameErr, setNameErr] = useState("");
   const [passwordErr, setPasswordErr] = useState("");
+  const [display, setDisplay] = useState(undefined)
+
+
+
+
 
   const onSubmit = (e) => {
     e.preventDefault();
     setNameErr("");
     setPasswordErr("");
-    if (name === "") {
+    if (formData.name=== "") {
       console.log(name);
       setNameErr("Username Required");
-
-      return false;
+    }else{
+      setNameErr("");
     }
 
-    if (password === "") {
+    if (formData.password === "") {
       setPasswordErr("Password required !");
 
       return false;
     } else {
-      if (password.length < 7) {
+      if (formData.password.length < 8) {
         setPasswordErr("Minimum 8 digit long password required !");
 
         return false;
       }
-      console.log(password.search(/[0-9]/));
-      if (Number(password.search(/[0-9]/)) < 1) {
+      console.log(formData.password.search(/[0-9]/));
+      if (Number(formData.password.search(/[0-9]/)) < 1) {
         setPasswordErr("Password must contain atleast one digit !");
 
         return false;
       }
     }
-
+setDisplay(true);
     return true;
   };
+
+
+  const handleCheck = (event) => {
+    var updatedList = [...formData.service];
+    if (event.target.checked) {
+      updatedList = [...formData.service, event.target.value];
+    } else {
+      updatedList.splice(formData.service.indexOf(event.target.value), 1);
+    }
+    console.log(event.target.value,event.target.checked);
+    setFormData({...formData,service:updatedList});
+    
+  };
+
   return (
     <div className="App">
       React Create form task
@@ -50,7 +83,7 @@ export default function App() {
             <input
               id="Username"
               type="text"
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => setFormData({...formData,name:e.target.value})}
             />
             {nameErr !== "" && (
               <span
@@ -73,7 +106,7 @@ export default function App() {
             <input
               id="password"
               type="password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setFormData({...formData,password:e.target.value})}
             />
             {passwordErr !== "" && (
               <span
@@ -92,75 +125,59 @@ export default function App() {
           <div style={{display: "flex",marginLeft:"-0.5px", alignItems:"center"}}>
       <lable class="lable">City of Employment </lable>
       <div>
-        <input id="password" type="text" />
+        <input id="city" type="text" onChange={(e) => setFormData({...formData,city:e.target.value})}/>
       </div>
     </div>
           <div>
             <lable for="#web_server">Web server</lable>
-            <select id="web_server">
-              <option value="--Choose a server--">--Choose a server--</option>
-              <option value="--Choose a server--">France</option>
-              <option value="--Choose a server--">Surat</option>
-              <option value="--Choose a server--">Bengluru</option>
+            <select id="web_server" onChange={(e) => setFormData({...formData,server:e.target.value})}>
+              <option value="">--Choose a server--</option>
+              <option value="France">France</option>
+              <option value="Surat">Surat</option>
+              <option value="Bengluru">Bengluru</option>
             </select>
           </div>
-          <fildset style={{margin:"20px 0px"}}  class="radio_wrapper">
+          <div style={{margin:"20px 0px"}}  class="radio_wrapper"  onChange={(e) => setFormData({...formData,role:e.target.value})}>
             <div class="radio_lable">
               <lable>Please specify your role</lable>
             </div>
             <div class="radio_group">
-              <div class="radio_btn">
-                <input type="radio" id="Engineer" value="" name="role"></input>
-                <label for="#Engineer">Admin</label>
-              </div>
-              <div class="radio_btn">
-                <input type="radio" id="Engineer" value="" name="role"></input>
-                <label for="#Engineer">Engineer</label>
-              </div>
-              <div class="radio_btn">
-                <input type="radio" id="Engineer" value="" name="role"></input>
-                <label for="#Engineer">Manager</label>
-              </div>
-              <div class="radio_btn">
-                <input type="radio" id="Engineer" value="" name="role"></input>
-                <label for="#Engineer">Guest</label>
-              </div>
+              {
+                radioList.map(val=>{
+                  return(
+                  <div  class="radio_btn">
+                  <input key={val} type="radio" id="Engineer" value={val} name="role"></input>
+                   <label for="#Engineer">{val}</label>
+                 </div>
+                  )
+                })
+              }
             </div>
-          </fildset>
+          </div>
         
           <fildset style={{marginTop:" 10px"}} class="radio_wrapper">
             <div class="radio_lable">
               <lable>Single Sign-on to the following</lable>
             </div>
             <div class="radio_group">
-              <div class="radio_btn">
-                <input
-                  type="checkbox"
-                  id="Engineer"
-                  value=""
-                  name="role"
-                ></input>
-                <label for="#Engineer">Mail</label>
-              </div>
-              <div class="radio_btn">
-                <input
-                  type="checkbox"
-                  id="Engineer"
-                  value=""
-                  name="role"
-                ></input>
-                <label for="#Engineer">Payroll</label>
-              </div>
-              <div class="radio_btn">
-                <input
-                  type="checkbox"
-                  id="Engineer"
-                  value=""
-                  name="role"
-                ></input>
-                <label for="#Engineer">Self-service</label>
-              </div>
-            </div>
+{
+  checkList.map(val=>{
+    return(
+      <div key={val} class="radio_btn">
+      <input
+        type="checkbox"
+        id="Engineer"
+        value={val}
+        name="role"
+        onChange={handleCheck}
+      ></input>
+      <label for="#Engineer">{val}</label>
+    </div>
+    )
+  })
+}
+</div>
+
           </fildset>
 
           <div class="btn_holder">
@@ -171,7 +188,11 @@ export default function App() {
 
             <button class="btn"> Reset</button>
           </div>
-          <h1 id="tost">Login Sucessfull</h1>
+          <div style={{marginTop:"20px"}}>
+          {
+            display&&display===true&&  <DisplayData data={formData}></DisplayData>
+          }
+        </div>
         </div>
       </form>
     </div>

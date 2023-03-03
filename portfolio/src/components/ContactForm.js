4a77978bc637135fec24b65a46f431c8163d16ee
Prathamesh-Kulkarni01 <prathameshkulkarni710@gmail.com>
2023-03-03 10:45:57 +0530

    Menu IconClick issue solved .... Code rectified

--- portfolio/src/components/ContactForm.js
+++ portfolio/src/components/ContactForm.js
@@ -1,27 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const ContactForm = () => {
   return (
     <div>
-        <div class="">
-                 <div >
-                    <div style={{display:"flex",flexDirection:"column",alignItems: "center",padding: "10px"}}>
-                     
-                        <input style={{borderRadius: "10px",padding: "10px",width: "320px",margin: "10px"}} id="Username" type="text" placeholder="Enter Your Mail" />
-                        
-                          <input style={{borderRadius: "10px",padding: "10px",width: "320px",height: "100px",margin: "10px"}} id="Username" type="text" placeholder="Leave your message here..."/>
-                       
-                      <button style={{borderRadius: "10px",padding: "10px",width: "200px"}}> Send</button>
-                 </div>
-                
-            
-              
-         
-          </div>
-    
-        </div>
-    </div>
-  )
-}
+      <div className="">
+        <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "10px",
+            }}
+          >
+            <input
+              style={{
+                borderRadius: "10px",
+                padding: "10px",
+                width: "320px",
+                margin: "10px",
+              }}
+              id="Username"
+              type="text"
+              placeholder="Enter Your Mail"
+            />
 
-export default ContactForm
+            <input
+              style={{
+                borderRadius: "10px",
+                padding: "10px",
+                width: "320px",
+                height: "100px",
+                margin: "10px",
+              }}
+              id="Username"
+              type="text"
+              placeholder="Leave your message here..."
+            />
+
+            <button
+              style={{ borderRadius: "10px", padding: "10px", width: "200px" }}
+            >
+              {" "}
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- portfolio/src/components/WorkItem.js
+++ portfolio/src/components/WorkItem.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const WorkItem = (props) => {
   return (
     <div>
-        <div class="about_body " >
-              <p> <strong>Role:</strong> React Develor Intern</p>
-              <p> <strong>Company:</strong> Axelor India Private Limited</p>
-              <p><span/><strong>Duration:</strong> 02/2023-05/2023(3 Month)</p>
-            </div>
-              <br></br>
+      <div className="about_body ">
+        <p>
+          {" "}
+          <strong>Role:</strong> {props.role}
+        </p>
+        <p>
+          {" "}
+          <strong>Company:</strong> {props.comp}
+        </p>
+        <p>
+          <span />
+          <strong>Duration:</strong>{props.dur}
+        </p>
+      </div>
+      <br></br>
     </div>
-  )
-}
+  );
+};
 
-export default WorkItem
+export default WorkItem;
